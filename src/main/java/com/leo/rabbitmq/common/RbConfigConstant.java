ed261d68cb2c665f14a17cb2ba338cff6adf4af5
李行 lihang (61448) <lihang@tuniu.com>
2023-04-16 16:26:27 +0800

    代码优化

--- src/main/java/com/leo/rabbitmq/common/RbConfigConstant.java
+++ src/main/java/com/leo/rabbitmq/common/RbConfigConstant.java
@@ -2,10 +2,32 @@ package com.leo.rabbitmq.common;
 
 public class RbConfigConstant {
 
+    // direct
+
     public static final String EXCHANGE_NAME = "exchange_demo";
     public static final String QUEUE_NAME = "queue_demo";
     public static final String ROUTING_KEY = "routingkey_demo";
     public static final String QUEUE_NAME2 = "topicDemo1";
+
+    // topic
+
+    public static final String TOPIC_EXCHANGE_NAME = "topicExchange";
+    public static final String TOPIC_QUEUE_A = "topic.queue.a";
+    public static final String TOPIC_QUEUE_B = "topic.queue.b";
+    public static final String TOPIC_QUEUE_C = "topic.queue.c";
+
+    public static final String TOPIC_ROUTING_KEY_A = "topic.routingkey.a";
+    public static final String TOPIC_ROUTING_KEY_STAR = "topic.routingkey.*";
+    public static final String TOPIC_ROUTING_KEY_HASH = "topic.routingkey.#";
+
+    // fanout
+    public static final String FANOUT_EXCHANGE_NAME = "fanoutExchange";
+    public static final String FANOUT_QUEUE_A = "fanout.queue.a";
+    public static final String FANOUT_QUEUE_B = "fanout.queue.b";
     
-    
+    // headers
+    public static final String Headers_EXCHANGE_NAME = "headersExchange";
+    public static final String Headers_QUEUE_A = "headers.queue.a";
+    public static final String Headers_QUEUE_B = "headers.queue.b";
+
 }
---
+++ src/main/java/com/leo/rabbitmq/consume/FanoutConsumerA.java
@@ -0,0 +1,74 @@
+package com.leo.rabbitmq.consume;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import com.leo.rabbitmq.common.RbConfigConstant;
+import com.rabbitmq.client.Channel;
+
+@Component
+ public class FanoutConsumerA{
+    Logger logger = org.slf4j.LoggerFactory.getLogger(FanoutConsumerA.class);
+
+    private ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<String, Integer>();
+
+    enum Action {
+        SUCCESS, RETRY, REJECT
+    }
+
+    @RabbitHandler
+    @RabbitListener(queuesToDeclare = @Queue(RbConfigConstant.FANOUT_QUEUE_A))
+    public void receive(Map<String, Object> msgMap, Message message,Channel channel) {
+        long tag = message.getMessageProperties().getDeliveryTag();
+        Action action = Action.SUCCESS;
+        try {
+            String msg =msgMap.get("msg").toString();
+            String msgId = msgMap.get("msgId").toString();
+            logger.info("receive msg:{},tag:{},msgId:{}", msg,tag,msgId);
+            if ("bad".equals(msg) && map.get(msgId) == null) {
+                map.put(msgId, 1);
+                throw new IllegalArgumentException("bad msg,抛出可重回队列的异常");
+            }
+            if(map.get(msgId) != null){
+                logger.info("重试次数:{}",map.get(msgId));
+            }
+            if ("error".equals(msg)) {
+                throw new RuntimeException("error msg,抛出不可重回队列的异常");
+            }
+        } catch (IllegalArgumentException e) {
+            logger.error("IllegalArgumentException:{}", e.getMessage());
+            action = Action.REJECT;
+        } catch (Exception e) {
+            logger.error("Exception:{}", e.getMessage());
+            action = Action.REJECT;
+        } finally {
+            try {
+                switch (action) {
+                    case SUCCESS:
+                        channel.basicAck(tag, false);
+                        break;
+                    case RETRY:
+                        channel.basicNack(tag, false, true);
+                        break;
+                    case REJECT:
+                        channel.basicNack(tag, false, false);
+                        break;
+                    default:
+                        break;
+                }
+                channel.close();
+            } catch (Exception e2) {
+                logger.error("finally Exception:{}", e2.getMessage());
+            }
+        }
+
+    }
+    
+ }
